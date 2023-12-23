7001f6e2806b8f0c00a322f1c4db241fb463c2a4
naseer4864 <naseernoor4864@gmail.com>
2023-12-23 08:02:15 +0100

    added about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,34 @@
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-5" >
+      <h1 className="text-xl font-medium text-amber-600">About</h1>
+      <p>
+        IMDb, or the Internet Movie Database, has become a pivotal platform for
+        film, television, and video game enthusiasts since its inception in
+        1990.
+      </p>
+
+      <p>
+      Developed by Col Needham, IMDb serves as a comprehensive repository of
+      information, offering details such as cast and crew information, release
+      dates, production companies, genres, trivia, and user ratings for an
+      extensive collection of titles. One of IMDb's distinctive features is its
+      reliance on user-generated content. Registered users contribute
+      information, rate movies, write reviews, and participate in discussion
+      forums, creating a vibrant community of movie enthusiasts with diverse
+      perspectives on cinematic works.
+      </p>
+
+      <p>
+      service, tailored for industry insiders, provides additional features such
+      as contact information for professionals, advanced search options, and
+      analytics tools. IMDb's influence extends beyond its web presence, with
+      mobile apps offering a seamless experience for users on the go. The
+      platform's continuous evolution includes features like the IMDb Top 250
+      list, curated lists, awards information, and streaming recommendations. In
+      </p>
+    </div>
+  );
+};
+
+export default About;
