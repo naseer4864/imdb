aa0ddf64e1403d5b0bcaa73d9462ad1a70b8a444
naseer4864 <naseernoor4864@gmail.com>
2023-12-27 21:54:57 +0100

    fix

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-      <div className="flex justify-center items-center w-full h-16 md:h-14 mt-5 dark:bg-gray-600 bg-amber-100 p-2 md:p-0">
+      <div className="flex justify-center items-center w-full h-16 md:h-14 mt-5 dark:bg-gray-600 bg-amber-100 p-2">
         <p className="text-xs md:text-sm">
           &copy; 2023. All rights reserved. Designed by NaseerCode.
         </p>
